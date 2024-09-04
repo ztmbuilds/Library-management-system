ab4a2328c1d5506c96f6df8f4b18ddcaf788f9f4
Zetro <s.zetro007@gmail.com>
2024-09-04 16:25:49 +0100

    fix

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,7 +1,68 @@
+import { IUser } from '../models/user.model';
 export interface QueryString {
   page?: string;
   sort?: string;
   limit?: string;
   fields?: string;
   [key: string]: string; //Index signature
+}
+
+declare global {
+  namespace Express {
+    export interface Request {
+      user?: {
+        role?: UserRole;
+        id: string;
+      };
+    }
+  }
+}
+
+// declare module 'express-serve-static-core' {
+//   interface Request {
+//     user?: IUser;
+//   }
+// }
+
+//User
+
+export interface IUserSignup {
+  username: string;
+  email: string;
+  password: string;
+}
+
+export interface IUserLogin {
+  username_or_email: string;
+  password: string;
+}
+
+export interface IUserUpdate {
+  username: string;
+  email: string;
+}
+
+export interface RefreshTokenDecoded extends Jwt.JwtPayload {
+  refreshToken: string;
+  id: string;
+}
+
+export interface IVerifyEmail {
+  userId: string;
+  verifyToken: string;
+}
+
+export enum UserRole {
+  ADMIN = 'admin',
+  USER = 'user',
+}
+
+//Book
+export interface UpdateBookInput {
+  title: string;
+  author: string;
+  genre: string;
+  availabe: boolean;
+  totalCopies: number;
+  availableCopies: number;
 }
