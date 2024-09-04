cbd961c0ba9c00e7e277e4b9929a25563c80b2d0
Zetro <s.zetro007@gmail.com>
2024-09-04 15:15:59 +0100

    fix

--- src/middlewares/validation-middleware.ts
+++ src/middlewares/validation-middleware.ts
@@ -1,14 +1,24 @@
 import { validationResult, ValidationChain } from 'express-validator';
 import { NextFunction, Request, Response } from 'express';
+import { AppError } from './error.middleware';
 
 export default function validate(rules: ValidationChain[]) {
   return async (req: Request, res: Response, next: NextFunction) => {
-    rules.map((rule) => rule.run(req));
+    await Promise.all(rules.map((rule) => rule.run(req)));
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
-      console.log(errors);
-      return next(errors);
+      return next(
+        new AppError(
+          errors
+            .array()
+            .map((err) => err.msg)
+            .join('. '),
+          400
+        )
+      );
+    } else {
+      next();
     }
-    next();
   };
 }
